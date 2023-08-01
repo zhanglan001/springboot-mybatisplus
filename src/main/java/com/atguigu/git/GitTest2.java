7781c1cbabc1af8be86973ded0f6fc9e9a1cd958
zl <15982612435@163.com>
2023-08-01 15:40:53 +0800

    idea second commit

--- src/main/java/com/atguigu/git/GitTest2.java
+++ src/main/java/com/atguigu/git/GitTest2.java
@@ -2,6 +2,7 @@ package com.atguigu.git;
 
 public class GitTest2 {
     public static void main(String[] args) {
-        System.out.println("gittest2");
+        System.out.println("git test1");
+        System.out.println("git test2");
     }
 }
