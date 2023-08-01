e7b028fd401810b85bf7611cb2944b72d40ba8eb
zl <15982612435@163.com>
2023-08-01 15:41:20 +0800

    idea third commit

--- src/main/java/com/atguigu/git/GitTest2.java
+++ src/main/java/com/atguigu/git/GitTest2.java
@@ -4,5 +4,6 @@ public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("git test1");
         System.out.println("git test2");
+        System.out.println("git test3");
     }
 }
