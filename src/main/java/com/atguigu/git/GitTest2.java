08f4372e348055354d30d319706e729ba34826e2
zl <15982612435@163.com>
2023-08-02 16:33:10 +0800

    push test

--- src/main/java/com/atguigu/git/GitTest2.java
+++ src/main/java/com/atguigu/git/GitTest2.java
@@ -8,5 +8,6 @@ public class GitTest2 {
         System.out.println("hot-fix");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
